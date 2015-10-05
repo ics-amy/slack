ee51d47512ef262048ea969f969db9971a964080
Amy Li <amyli@dhcp-168-105-83-148.wireless.manoa.hawaii.edu>
2015-10-05 09:41:00 -1000

    slack

---
+++ client/app.js
@@ -0,0 +1,15 @@
+/**
+ * Created by amyli on 10/5/15.
+ */
+
+Template.messages.helpers({
+  messages: [
+    { text: "All these messages" },
+    { text: "Uses the same template" },
+    { text: "But have a different data context" },
+    { text: "It's why these messages are all different!" },
+    { text: "Hey!" },
+    { text: "What's up man!" },
+    { text: "Hello" }
+  ]
+});
